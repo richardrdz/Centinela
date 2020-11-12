11418aaf4e2a8ce2c7aad14864c5a09a6fb3974e
richard <richardrdz97@gmail.com>
2020-11-11 23:32:53 -0600

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 uri2 = URI.parse("redis://redistogo:c78cdb75e5e7d4bc1ea1988ba8a87eb4@dory.redistogo.com:9004/")
-REDIS = Redis.new(:url => uri2)
+REDIS = Redis.new(url: "redis://redistogo:c78cdb75e5e7d4bc1ea1988ba8a87eb4@dory.redistogo.com:9004/"")
